573300dbeb32e65ad76849727af4f57a669e611f
MC_XiaoHei <xiaohei.xor7studio@foxmail.com>
2023-08-22 14:49:06 +0800

    Feat(About): add About page

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -2,16 +2,15 @@
   <q-page class="row items-center justify-evenly">
     <div class="q-pa-md row container">
       <q-card class="col-md" style="background-color: #e6f4ff;min-width: 350px">
-      <q-item>
-        <q-item-section>
-          <q-item-label class="text-h6">关于本站</q-item-label>
-        </q-item-section>
-        <q-item-section>
-          <q-item-label>本站致力于让妈粉能<b>享受</b>到和梦妈一样的坐牢体验！</q-item-label>
-          <q-item-label>开发者就只有MC_XiaoHei一个uwu</q-item-label>
-        </q-item-section>
-      </q-item>
-    </q-card>
+        <div class="q-pa-md" >
+          <b class="text-h5">关于本站</b>
+          <div style="height: 1rem"/>
+          <p>
+            本站致力于让妈粉能 <b>享受</b> 到和梦妈一样的坐牢体验！<br/>
+            开发者就只有MC_XiaoHei一个uwu
+          </p>
+        </div>
+      </q-card>
     </div>
   </q-page>
 </template>
